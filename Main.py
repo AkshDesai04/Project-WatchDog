8f9152da62da49e81955ab176cc5790dde34017d
AkshDesai04 <dev.akshdesai@gmail.com>
2022-11-14 07:32:08 +0530

    Added ReadMe.md

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import threading
 import time
 import datetime
-
 import numpy as np
 import cv2
 import pyautogui
+import os
+# import face_recognition
 
 def work():
 	i = ""
@@ -18,12 +19,39 @@ def work():
 def screenshot(i):
 	image = pyautogui.screenshot()
 	image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
-	cv2.imwrite("D:\\Shots\\Screenshot-" + i + ".png", image)
+	
+	cv2.imwrite("D:\\Shots\\Screenshot---" + i + ".png", image)
 
 def selfie(i):
 	# TODO: Work on this
 	print("selfie")
 
+def compress():
+	path = "D:\\Shots"
+	dir_list = os.listdir(path)
+	for i in range(len(dir_list)):
+		if dir_list[i].index('---red') == -1:
+			print("Doing " + dir_list[i])
+	print("Done")
+
+def face_rec():
+	path = "C:\\Users\\Aksh Desai\\Videos\\Test\\Test Data"
+	dir_list = os.listdir(path)
+	for i in range(len(dir_list)):
+		known_image = face_recognition.load_image_file("C:\\Users\\Aksh Desai\\Videos\\Test\\Known People\\Rachel.jpg")
+		unknown_image = face_recognition.load_image_file("C:\\Users\\Aksh Desai\\Videos\\Test\\Test Data\\" + dir_list[i])
+
+		biden_encoding = face_recognition.face_encodings(known_image)[0]
+		unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+		results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+
+		print(results)
+
 if __name__ == "__main__":
-	t1 = threading.Thread(target=work, args=())
+	t1 = threading.Thread(target=work)
 	t1.start()
+	# # t2 = threading.Thread(target=compress)
+	# # t2.start()
+	# t3 = threading.Thread(target=face_rec)
+	# t3.start()
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+1. TODO: Make Key logger
+2. TODO: Selfie
+    1. Face Recognition
+    2. Lock Laptop for Blocked People
+    3. Say hi if allowed user is detected
+3. TODO: Make a UI
+    1. Scrubbable Timeline
+4. Detect shut down(or similar system power events) event
+5. 
