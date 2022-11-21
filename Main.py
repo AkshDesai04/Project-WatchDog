c7d9f8d43235032aae71f561eef1308c87844c61
AkshDesai04 <dev.akshdesai@gmail.com>
2022-11-21 08:03:40 +0530

    Cleaned up Main.py

--- Main.py
+++ Main.py
@@ -5,19 +5,15 @@ import numpy as np
 import cv2
 import pyautogui
 import os
-# import face_recognition
-
-# import Config.py
 
 def work():
 	i = ""
 	while True:
 		current_time = datetime.datetime.now()
 		i = str(current_time.day) + "d-" + str(current_time.month) + "m-" + str(current_time.year) + "y---" + str(current_time.hour) + "hours" + "-" + str(current_time.minute) + "-" + "mins" + str(current_time.second) + "-" + "secs"
-		# screenshot(i) # temp
-		# selfie(i)
-		# time.sleep(int(get_text("time")))
-		print(get_text("time"))
+		screenshot(i)
+		selfie(i)
+		time.sleep(10)
 
 def screenshot(i):
 	image = pyautogui.screenshot()
@@ -25,31 +21,14 @@ def screenshot(i):
 	
 	cv2.imwrite("D:\\Shots\\Screenshot---" + i + ".png", image)
 
-# def selfie(i):
-# 	# TODO: Work on this
-# 	print("selfie")
+def selfie(i):
+	print("Taking a selfie")
 
-# def compress():
-# 	path = "D:\\Shots"
-# 	dir_list = os.listdir(path)
-# 	for i in range(len(dir_list)):
-# 		if dir_list[i].index('---red') == -1:
-# 			print("Doing " + dir_list[i])
-# 	print("Done")
+def compress():
+	print("Compressing images")
 
-# def face_rec():
-# 	path = "C:\\Users\\Aksh Desai\\Videos\\Test\\Test Data"
-# 	dir_list = os.listdir(path)
-# 	for i in range(len(dir_list)):
-# 		known_image = face_recognition.load_image_file("C:\\Users\\Aksh Desai\\Videos\\Test\\Known People\\Rachel.jpg")
-# 		unknown_image = face_recognition.load_image_file("C:\\Users\\Aksh Desai\\Videos\\Test\\Test Data\\" + dir_list[i])
-
-# 		biden_encoding = face_recognition.face_encodings(known_image)[0]
-# 		unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-
-# 		results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-
-# 		print(results)
+def face_rec():
+	print("Running face rec")
 
 
 
@@ -66,7 +45,3 @@ def get_text(str):
 if __name__ == "__main__":
 	t1 = threading.Thread(target=work)
 	t1.start()
-	# # t2 = threading.Thread(target=compress)
-	# # t2.start()
-	# t3 = threading.Thread(target=face_rec)
-	# t3.start()
