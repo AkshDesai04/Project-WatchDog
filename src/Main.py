934eaa3bcf039efa8f468c26f099710f941cd821
Aksh Desai <dev.akshdesai@gmail.com>
2023-01-20 17:12:58 +0530

    Cleaned and ordanised code

--- src/Main.py
+++ src/Main.py
@@ -1,63 +1,28 @@
-import time
 import datetime
 import os
-import threading
-import pyautogui
 import cv2
-import numpy as np
-import pylab
+import time
 
-path = "D:\\WatchdogTesting\\"
-folder = ""
-
-def work():
-	i = ""
-	while True:
-		current_time = datetime.datetime.now()
-		i = str(current_time.hour) + "hours" + "-" + str(current_time.minute) + "-" + "mins" + str(current_time.second) + "-" + "secs"
-		folder = str(current_time.day) + "d-" + str(current_time.month) + "m-" + str(current_time.year) + "y\\"
-		if not os.path.exists(path + folder):
-			os.makedirs(path + folder)
-			print("making folder")
-		screenshot(i, folder)
-		# selfie(i, folder)
-		time.sleep(1)
-
-def screenshot(i, folder):
-    image = pyautogui.screenshot()
-    image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
-    cv2.imwrite(path + folder + "\\Screenshot---" + i + ".png", image)
-    print("SS Taken")
-
-def selfie(i, folder):
-	cam_port = 0
-	cam = cv2.VideoCapture(1,cv2.CAP_DSHOW)
-	# cam = cv2.VideoCapture(0)
-
-	result, image = cam.read()
-
-	if result:
-		cv2.imwrite(path + folder + "\\WebCam---" + i + ".png", image)
-	else:
-		print("No image detected. Please! try again")
-
-	print("selfie taken")
-
-def compress():
-	pass
-
-def face_rec():
-	pass
-
-def get_text(str):
-    f = open("config.config.config", "r")
-    for x in f:
-        if x.startswith(str):
-            return x.split("=")[1].strip()
-    return "output"
+import Screenshot
+import Selfie
 
 
 
-if __name__ == "__main__": 
-	t1 = threading.Thread(target=work)
-	t1.start()
+print("hi, init taking place.\nJust a moment...")
+path = "D:\\WatchdogTesting\\" #Path is the folder where all data will be stored
+folder = "" #Folder is the path to the folder where todays files will be stored. It is within path's folder
+i = "" #i stores the date, time and other into
+
+while True:
+    current_time = datetime.datetime.now()
+    i = str(current_time.hour) + "hours" + "-" + str(current_time.minute) + "-" + "mins" + str(current_time.second) + "-" + "secs"
+    folder = str(current_time.day) + "d-" + str(current_time.month) + "m-" + str(current_time.year) + "y\\"
+    if not os.path.exists(path + folder):
+        os.makedirs(path + folder)
+        print("making folder")
+    ssfn = path + folder + "Screenshot---" + i + ".png" # screenshot file name
+    # sffn = path + folder + "\\Selfie---" + i + ".png"     #selfie file name
+    print(ssfn)
+    cv2.imwrite(ssfn, Screenshot.screenshot(i, folder))
+    # cv2.imwrite(sffn, Selfie.selfie(i, folder))
+    time.sleep(2.5)
---
+++ src/Screenshot.py
@@ -0,0 +1,10 @@
+import pyautogui
+import cv2
+import numpy as np
+
+
+def screenshot(i, folder):
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
+    print("Screenshot Taken")
+    return image
