ffff0314ca2ee1d72f854cb0b9184a932fb9dabe
Aksh Desai <dev.akshdesai@gmail.com>
2023-08-05 03:34:42 +0530

    UNSTABLE AND INCOMPLETE: Added database_operations file with basic functions

---
+++ src/database_operations.py
@@ -0,0 +1,45 @@
+import mysql.connector
+
+def create_db(connection, name):
+    pass
+
+
+def create_table(con, name, fields, type, size):
+    #fields, type, and size are arrays
+    pass
+
+
+def create_connection(host, user, password, database):
+    mydb = mysql.connector.connect(
+        host      =  host,
+        user      =  user,
+        password  =  password,
+        database  =  database
+    )
+    return mydb
+
+
+def db_insert(con, table, *data):
+    mycursor = con.cursor()
+
+    _data = ""
+    for i in data:
+        _data + ", " + i
+    _data = _data[:-2]
+
+    sql = f"INSERT INTO customers VALUES ({_data})"
+    mycursor.execute(sql)
+    con.commit()
+
+
+def db_read(con, query):
+    mycursor = con.cursor()
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+
+    return mycursor
+
+
+def db_execute_cmd(con, quercmd):
+    mycursor = con.cursor()
+    mycursor.execute(cmd)
