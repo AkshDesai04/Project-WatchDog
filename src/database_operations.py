9b2235d3ca9d6a61d341051b1ca1e9954b2c2fa8
Aksh Desai <dev.akshdesai@gmail.com>
2023-08-21 01:51:22 +0530

    Added insert data

--- src/database_operations.py
+++ src/database_operations.py
@@ -1,12 +1,12 @@
 import mysql.connector
 
-def create_db(connection, name):    #Done
-    mycursor = connection.cursor()
+def create_db(conn, name):    #Done
+    mycursor = conn.cursor()
     sql = f"CREATE DATABASE {name}"
 
     try:
         mycursor.execute(sql)
-        connection.commit()
+        conn.commit()
 
         print("DataBase created successfully!")
 
@@ -16,8 +16,8 @@ def create_db(connection, name):    #Done
     mycursor.close()
 
 
-def create_table(connection, name, fields, types, sizes, constraints): #Done
-    mycursor = connection.cursor()
+def create_table(conn, name, fields, types, sizes, constraints): #Done
+    mycursor = conn.cursor()
 
     #For Testing
     # if len(fields) != len(types) or len(types) != len(sizes) or len(sizes) != len(constraints):
@@ -35,7 +35,7 @@ def create_table(connection, name, fields, types, sizes, constraints): #Done
     sql = f"CREATE TABLE {name} (" + columns + ")"
     try:
         mycursor.execute(sql)
-        connection.commit()
+        conn.commit()
         print(sql)
         
         print("Table created successfully!")
@@ -46,7 +46,7 @@ def create_table(connection, name, fields, types, sizes, constraints): #Done
     mycursor.close()
 
 
-def create_connection(host, user, password, database):
+def create_conn(host, user, password, database):
     mydb = mysql.connector.connect(
         host      =  host,
         user      =  user,
@@ -56,20 +56,42 @@ def create_connection(host, user, password, database):
     return mydb
 
 
-def db_insert(con, table, *data):
-    mycursor = con.cursor()
+def db_insert(conn, table, *data): #Done
+    mycursor = conn.cursor()
+    
+    _fields = []
+    _datas = []
 
-    _data = ""
-    for i in data:
-        _data + ", " + i
-    _data = _data[:-2]
+    for key, value in data.items():
+        _fields.append(key)
+        if isinstance(value, str):
+            _datas.append(f"'{value}'")
+        else:
+            _datas.append(str(value))
 
-    sql = f"INSERT INTO customers VALUES ({_data})"
-    mycursor.execute(sql)
-    con.commit()
+    fields = ", ".join(_fields)
+    datas = ", ".join(_datas)
+    print(fields)
+    print(datas)
+
+    sql = f"insert into {table} ({fields}) values ({datas})"
+
+    try:
+        mycursor.execute(sql)
+        conn.commit()
+        print("sql: " + sql)
+        print("fields: " + fields)
+        print("datas: " + datas)
+        
+        print("Data inserted!")
+
+    except mysql.connector.Error as err:
+        print("Error:", err)
+
+    mycursor.close()
 
 
-def db_read(con, query):
+def db_read(conn, query):
     mycursor = con.cursor()
     mycursor.execute(query)
     myresult = mycursor.fetchall()
@@ -77,6 +99,6 @@ def db_read(con, query):
     return mycursor
 
 
-def db_execute_cmd(con, quercmd):
+def db_execute_cmd(conn, quercmd):
     mycursor = con.cursor()
     mycursor.execute(cmd)
