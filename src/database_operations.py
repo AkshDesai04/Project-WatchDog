ccdf9607efb1f473866696330ab8fed292b14d97
Aksh Desai <dev.akshdesai@gmail.com>
2024-01-03 10:16:51 +0530

    UNSTABLE: Moving Database to SQLite

--- src/database_operations.py
+++ src/database_operations.py
@@ -1,65 +1,77 @@
-import mysql.connector
+# import mysql.connector
+import sqlite3
 
 
-
-def create_db(conn, name): #Done
-    mycursor = conn.cursor()
-    sql = f"CREATE DATABASE {name}"
-
-    try:
-        mycursor.execute(sql)
-        conn.commit()
-
-        print("DataBase created successfully!")
-
-    except mysql.connector.Error as err:
-        print("Error:", err)
-
-    mycursor.close()
-
-
-def create_table(conn, name, fields, types, sizes, constraints): #Done
-    mycursor = conn.cursor()
-
-    #For Testing
-    # if len(fields) != len(types) or len(types) != len(sizes) or len(sizes) != len(constraints):
-        # return "Input arrays must have the same length"
-    #For Testing
-
-    descriptions = []
-    for i in range(len(fields)):
-        desc = f"{fields[i]} {types[i]}({sizes[i]})"
-        if constraints[i]:
-            desc += " " + " ".join(constraints[i])
-        descriptions.append(desc)
-    columns = ", ".join(descriptions)
-
-    sql = f"CREATE TABLE {name} (" + columns + ")"
-    try:
-        mycursor.execute(sql)
-        conn.commit()
-        print(sql)
-        
-        print("Table created successfully!")
-
-    except mysql.connector.Error as err:
-        print("Error:", err)
-
-    mycursor.close()
+def create_db(name): #WorkedOn
+    # mycursor = conn.cursor()
+    # sql = f"CREATE DATABASE {name}"
+    #
+    # try:
+    #     mycursor.execute(sql)
+    #     conn.commit()
+    #
+    #     print("DataBase created successfully!")
+    #
+    # except mysql.connector.Error as err:
+    #     print("Error:", err)
+    #
+    # mycursor.close()
+    connection = sqlite3.connect(name)
 
 
-def create_conn(host, user, password, database = ""): #Done
-    conn = mysql.connector.connect(
-        host      =  host,
-        user      =  user,
-        password  =  password,
-        database = database
-    )
+def create_table(conn, name, fields, types, sizes, constraints): #WorkedOn
+    # mycursor = conn.cursor()
+    #
+    # #For Testing
+    # # if len(fields) != len(types) or len(types) != len(sizes) or len(sizes) != len(constraints):
+    #     # return "Input arrays must have the same length"
+    # #For Testing
+    #
+    # descriptions = []
+    # for i in range(len(fields)):
+    #     desc = f"{fields[i]} {types[i]}({sizes[i]})"
+    #     if constraints[i]:
+    #         desc += " " + " ".join(constraints[i])
+    #     descriptions.append(desc)
+    # columns = ", ".join(descriptions)
+    #
+    # sql = f"CREATE TABLE {name} (" + columns + ")"
+    # try:
+    #     mycursor.execute(sql)
+    #     conn.commit()
+    #     print(sql)
+    #
+    #     print("Table created successfully!")
+    #
+    # except mysql.connector.Error as err:
+    #     print("Error:", err)
+    #
+    # mycursor.close()
+    curr = conn.cursor()
+    curr.execute(f"drop table if exists {name}")
+
+    query = f"create table {name} ("
+    for i in range(0, fields.size()):
+        query = f"\n{query} {fields[i]} {types[i]}({sizes[i]}) {constraints},"
+    query = f"{query[:len(query) - 1]} );"
+
+    print(query)
+    conn.execute(query)
+
+def create_conn(name): #WorkedOn
+    # conn = mysql.connector.connect(
+    #     host      =  host,
+    #     user      =  user,
+    #     password  =  password,
+    #     database = database
+    # )
+    # return conn
+    conn = sqlite3.connect(name)
     return conn
 
 
-def db_insert(conn, table, *data): #Done
-    mycursor = conn.cursor()
+def db_insert(table, *data): #WorkedOn NoUpdatesMade
+    curr = conn.cursor()
     
     _fields = []
     _datas = []
@@ -79,7 +91,7 @@ def db_insert(conn, table, *data): #Done
     sql = f"insert into {table} ({fields}) values ({datas})"
 
     try:
-        mycursor.execute(sql)
+        curr.execute(sql)
         conn.commit()
         print("sql: " + sql)
         print("fields: " + fields)
@@ -90,27 +102,32 @@ def db_insert(conn, table, *data): #Done
     except mysql.connector.Error as err:
         print("Error:", err)
 
-    mycursor.close()
+    curr.close()
 
 
-def db_read(conn, query): #Done
-    try:
-        cursor = conn.cursor()
-        cursor.execute(query)
-        
-        if cursor.with_rows:
-            result = cursor.fetchall()
-            if len(result) == 1 and len(result[0]) == 1:
-                return result[0][0]
-            else:
-                return result
-        else:
-            return None
-    except Exception as e:
-        return str(e)
+def db_read(conn, query): #WorkedOn
+    curr = conn.cursor()
+    curr.execute(query)
+    return curr.fetchall()
+    # try:
+    #     curr = conn.cursor()
+    #     curr.execute(query)
+    #
+    #     if cursor.with_rows:
+    #         result = cursor.fetchall()
+    #         if len(result) == 1 and len(result[0]) == 1:
+    #             return result[0][0]
+    #         else:
+    #             return result
+    #     else:
+    #         return None
+    # except Exception as e:
+    #     return str(e)
 
 
-def db_execute_cmd(conn, cmd): #Done
+
+
+def db_execute_cmd(conn, cmd): #Pending
     try:
         cursor = conn.cursor()
         cursor.execute(cmd)
